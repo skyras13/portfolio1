d5f6bc70ceef15b7af68e8bf69b1821fa940e648
Skyler S Rasmussen <skyrasmussen13@gmail.com>
2024-05-30 08:29:01 -0600

    Add React Router
    Added react router and pages rather than a single page with anchor links.

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,32 @@
 import styles from './styles'
-import {
-  Billing,
-  Business,
-  CardDeal,
-  Clients,
-  CTA,
-  Footer,
-  Navbar,
-  Stats,
-  Testimonials,
-  Hero,
-} from './components'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Navbar, Footer } from './components'
+import AboutMe from './pages/AboutMe'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+import Experience from './pages/Experience'
 
 const App = () => (
-  <div className='bg-primary w-full overflow-hidden'>
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Navbar />
+  <Router>
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+      <Routes>
+        <Route path='/' element={<AboutMe />} />{' '}
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/experience' element={<Experience />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Footer />
+        </div>
       </div>
     </div>
-
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
-      </div>
-    </div>
-
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Stats />
-        <Business />
-        <Billing />
-        <CardDeal />
-        <Testimonials />
-        <Clients />
-        <CTA />
-        <Footer />
-      </div>
-    </div>
-  </div>
+  </Router>
 )
 
 export default App
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,34 +7,9 @@ const Hero = () => (
   <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
     <div
       className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-      <div
-        className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient 
-            rounded-[10px] mb-2'>
-        <img src={discount} alt='discount' className='w-[32px] h-[32px]' />
-        <p className={`${styles.paragraph} l-2`}>
-          <span className='text-white'>20%</span> Discount For{' '}
-          <span className='text-white'>1 Month</span> Account
-        </p>
-      </div>
-
       <div className='flex flex-row justify-between items-center w-full'>
         <h1 className='flex-1 font-poppings font-semibold ss:text-[72px] text-[52px] animated-text'>
-          <span className='animated-letter'>S</span>
-          <span className='animated-letter'>k</span>
-          <span className='animated-letter'>y</span>
-          <span className='animated-letter'>l</span>
-          <span className='animated-letter'>e</span>
-          <span className='animated-letter'>r</span>{' '}
-          <span className='animated-letter'>R</span>
-          <span className='animated-letter'>a</span>
-          <span className='animated-letter'>s</span>
-          <span className='animated-letter'>m</span>
-          <span className='animated-letter'>u</span>
-          <span className='animated-letter'>s</span>
-          <span className='animated-letter'>s</span>
-          <span className='animated-letter'>e</span>
-          <span className='animated-letter'>n</span>
-          <br className='sm:block hidden' />
+          Skyras13 Software
         </h1>
         <div className='ss:flex hidden md:mr-4 mr-0'>
           <GetStarted />
